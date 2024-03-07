e4c745be38fa2eda97bbacc56c1916e51c13b4de
oleg shchepotkin <music.wave.form7@gmail.com>
2024-03-07 16:23:49 +0300

    (navbar): styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../style';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
+
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddindX} w-full flex 
+      items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between
+        items-center max-w-7xl mx-auto'
+      >
+        <Link
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0,0);}
+          }
+        >
+          <img src={logo} alt='logo' className='w-12 h-12 object-contain'></img>
+          <p className='text-white text-[18px] font-bold cursor-ponter'>
+            Oleg S. <span className='sm:block hidden'>| Frontend Developer</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
