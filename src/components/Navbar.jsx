8105d56ac05bc9cb19177ca40d7adebb23365b3f
oleg shchepotkin <music.wave.form7@gmail.com>
2024-03-14 17:20:57 +0300

    (hero): start making layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddindX} w-full flex 
+    <nav className={`${styles.paddingX} w-full flex 
       items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between
@@ -25,8 +25,8 @@ const Navbar = () => {
           }
         >
           <img src={logo} alt='logo' className='w-12 h-12 object-contain'></img>
-          <p className='text-white text-[18px] font-bold cursor-ponter'>
-            Oleg S. <span className='sm:block hidden'>| Frontend Developer</span>
+          <p className='text-white text-[18px] font-bold cursor-ponter flex'>
+            Oleg S. &nbsp;<span className='sm:block hidden'>| Frontend Developer</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -63,7 +63,10 @@ const Navbar = () => {
               {navLinks.map((link) =>
                 <li 
                   key={link.id}
-                  onClick={() => setActive(link.title)}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
                   className={`${
                     active === link.title
                       ? "text-white"
