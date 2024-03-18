5e97e3aa0c33e0666a69984075a623a9baa0f951
oleg shchepotkin <music.wave.form7@gmail.com>
2024-03-18 19:07:05 +0300

    (model): loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,6 +3,8 @@ import React, {Suspense, useEffect, useState} from 'react'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber';
 
+import CanvasLoader from "../Loader";
+
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
@@ -30,12 +32,14 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true}}
     >
 
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Computers />
+      </Suspense>
       <Preload all />
     </Canvas>
   )
