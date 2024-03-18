18766669ec7edd375a738e34f54381b221370a19
oleg shchepotkin <music.wave.form7@gmail.com>
2024-03-18 18:53:43 +0300

    (model): computer 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {Suspense, useEffect, useState} from 'react'
+
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
+
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={1}
+        groundColor="black" />
+      <pointLight intensity={1.2} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[0, -0.2, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true}}
+    >
+
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      <Preload all />
+    </Canvas>
+  )
+};
+
+export default ComputersCanvas;
