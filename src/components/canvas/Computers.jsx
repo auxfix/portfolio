24e6fa1461cca5e3d077954885b88ca0a4391533
oleg shchepotkin <music.wave.form7@gmail.com>
2024-03-20 17:51:31 +0300

    (light): add one more light sourse to computer scene

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,33 @@
-import React, {Suspense, useEffect, useState} from 'react'
+import React, {Suspense, useEffect, useState, useRef} from 'react'
 
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { SpotLightHelper, MeshPhongMaterial  } from 'three'
 
 import CanvasLoader from "../Loader";
 
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
     <mesh>
-      <hemisphereLight intensity={1.15}
-        groundColor="black" />
-      <pointLight intensity={1.25} />
-      <primitive
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[0, -0.2, -0.1]}
-      />
+        <hemisphereLight intensity={1.15}
+          groundColor="black" />
+        <directionalLight
+          position={[-10, 50, 10]} 
+          intensity={1.5} 
+          castShadow 
+          shadow-mapSize={[1024, 1024]}
+        />
+        <pointLight intensity={1.25} /> 
+        <primitive
+          object={computer.scene}
+          scale={0.75}
+          position={[0, -3.25, -1.5]}
+          rotation={[0, -0.2, -0.1]}
+          material={new MeshPhongMaterial({ color: 0xffffff, shininess: 100 })}
+        />
     </mesh>
   )
 }
@@ -36,7 +45,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-        />
+        /> 
         <Computers />
       </Suspense>
       <Preload all />
