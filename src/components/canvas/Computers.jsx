f1a5dea3d7f1eb2c2aeacc90fbd9536ad3f590c5
oleg shchepotkin <music.wave.form7@gmail.com>
2024-05-02 23:49:39 +0300

    (mobile): add responsive position

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -24,7 +24,7 @@ const Computers = ({ isMobile }) => {
         <primitive
           object={computer.scene}
           scale={isMobile ? 0.48 : 0.75}
-          position={[0, -3.25, -1.5]}
+          position={isMobile ?[0, -2, -1.5]:[0, -3.25, -1.5]}
           rotation={[0, -0.2, -0.1]}
           material={new MeshPhongMaterial({ color: 0xffffff, shininess: 100 })}
         />
