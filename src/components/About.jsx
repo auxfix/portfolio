0559ad43fcb61e94d82f8a2879829d29974d64bd
oleg shchepotkin <music.wave.form7@gmail.com>
2024-05-13 16:37:26 +0300

    (oveview): firts steps

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import Tilt from 'react-tilt'
+import Title from 'react-title'
 import { motion } from 'framer-motion';
+
+import { styles } from '../style';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.heroHeadText}>
+          Overview
+        </h2>
+      </motion.div>
+    </>
   )
 }
 
