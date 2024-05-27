a02b515b3acb7cf929c284f82f73f014f0535319
oleg shchepotkin <music.wave.form7@gmail.com>
2024-05-27 16:58:43 +0300

    (style): expirince time line styling

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,16 +9,39 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
-const ExperienceCard = ({ experiences }) => {
-  <VerticalTimelineElement
+const ExperienceCard = ({ experience }) => (<VerticalTimelineElement
     contentStyle={{
-      backfaceVisibility: '#1d1836',
+      background: '#1d1836',
       color: '#fff'
     }}
+    contentArrowStyle={{
+      borderRight: '7px solid #232631'
+    }}
+    date={experience.date}
+    iconStyle={{
+      background: experience.iconBg
+    }}
+    icon={
+      <div className='flex justify-center
+        items-center w-full h-full
+      '>
+        <img
+          src={experience.icon}
+          alt={experience.company_name}
+          className='w-[60%] h-[60%]
+            object-contain
+          '
+        >
+        </img>
+      </div>
+    }
   >
-
-  </VerticalTimelineElement>
-}
+    <div>
+      <h3 className="text-white text-[24px]
+        font-bold
+      ">{experience.title}</h3>
+    </div>
+  </VerticalTimelineElement>)
 
 const Experience = () => {
   return (
@@ -34,10 +57,10 @@ const Experience = () => {
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
           {
-            experiences.map((experiences, index) =>(
+            experiences.map((experience, index) =>(
               <ExperienceCard 
                 key={index}
-                experiences={experiences}
+                experience={experience}
               />
             ))
           }
