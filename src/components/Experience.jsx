f72372904260a0a1f4ec060f14ac200f17ca6376
oleg shchepotkin <music.wave.form7@gmail.com>
2024-05-29 14:04:40 +0300

    (exp): job point

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -41,6 +41,19 @@ const ExperienceCard = ({ experience }) => (<VerticalTimelineElement
         font-bold
       ">{experience.title}</h3>
       <p className="text-secondary text-[16px] font-semibold" style={{ margin: 0}}>{experience.company_name}</p>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {
+          experience.points.map((point, index) => (
+            <li
+              key={`experience-pont-${index}`}
+              className='text-white-100 text-[14px] 
+              pl-1 tracking-wider'
+            >
+              {point}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   </VerticalTimelineElement>)
 
