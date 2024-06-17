3ddd84e1a227cb7d17dd5c16781cb3b0f2ca819a
oleg shchepotkin <music.wave.form7@gmail.com>
2024-06-17 15:53:06 +0300

    (tech): start activity

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {
+        technologies.map(t => (
+          <div className="h-28 w-28"
+            key={t.name}
+          >
+            <BallCanvas icon={t.icon}/>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, '');
