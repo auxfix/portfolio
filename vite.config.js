acc912d3721c12dbdafde6cafaebffa274e39edc
Oleg Shchepotkin <music.wave.form7@gmail.com>
2024-10-10 16:25:23 +0300

    (ci/cd): try to implement custom base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base:  '/portfolio/',
+    base:  env.PAGES_URL ? `/${env.PAGES_URL}/` : '/',
     plugins: [react()],
   }
 })
